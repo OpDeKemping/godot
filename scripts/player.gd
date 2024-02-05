7790b3f78672e8f8dcb72ab39fe4307f08a411b3
OpDeKemping <153827668+OpDeKemping@users.noreply.github.com>
2024-02-05 13:33:57 +0100

    Initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,20 @@
+extends RigidBody2D
+
+var wheels = []
+var speed = 60000
+var altspeed = -60000
+var maxspeed = 50
+
+
+func _ready():
+	wheels = get_tree().get_nodes_in_group("wheel")
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right") :
+		for wheels in wheels:
+			wheels.apply_torque_impulse(speed*delta*60)
+			
+	if Input.is_action_pressed("ui_left") :
+		for wheels in wheels:
+			wheels.apply_torque_impulse(altspeed*delta*60)
+
